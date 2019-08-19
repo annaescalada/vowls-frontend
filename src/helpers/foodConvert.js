5258cc0e247cd0ed5e5652777256219521d9796f
Anna Escalada <annaescalada@gmail.com>
2019-08-19 13:19:21 +0200

    cereal file updated in helpers

--- src/helpers/foodConvert.js
+++ src/helpers/foodConvert.js
@@ -3,7 +3,7 @@ export const foodGroups = ['cereals', 'proteins', 'tubers', 'fruit', 'berries', 
 
 export const foodGroupToIcon = (group => {
     const foodGroups = ['cereals', 'proteins', 'tubers', 'fruit', 'berries', 'cruciferous', 'greens', 'othervegs', 'omega', 'fat', 'dairy'];
-    const foodGroupsIcons = ['./images/Food-icons/cereals.png', './images/Food-icons/Protein.png', './images/Food-icons/Tubers.png', './images/Food-icons/Fruit.png', './images/Food-icons/Berries.png', './images/Food-icons/Cruciferous.png', './images/Food-icons/Greens.png', './images/Food-icons/Otherveg.png', './images/Food-icons/Omega.png', './images/Food-icons/Fat.png', './images/Food-icons/Dairy.png'];
+    const foodGroupsIcons = ['./images/Food-icons/Cereals.png', './images/Food-icons/Protein.png', './images/Food-icons/Tubers.png', './images/Food-icons/Fruit.png', './images/Food-icons/Berries.png', './images/Food-icons/Cruciferous.png', './images/Food-icons/Greens.png', './images/Food-icons/Otherveg.png', './images/Food-icons/omega.png', './images/Food-icons/Fat.png', './images/Food-icons/Dairy.png'];
     return foodGroupsIcons[foodGroups.indexOf(group)];
 });
 
